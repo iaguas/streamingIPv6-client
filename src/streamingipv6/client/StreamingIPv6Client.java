06d2ec4e6fbcee43d0aa8e9c09cffd8841064b42
DanielGSM <danigarciasan@hotmail.com>
2016-02-24 18:23:36 +0100

    Subida inicial del proyecto de Netbeans

---
+++ src/streamingipv6/client/StreamingIPv6Client.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streamingipv6.client;
+
+/**
+ *
+ * @author rsng06
+ */
+public class StreamingIPv6Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
